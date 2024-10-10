7633e254e2e8a7b7b976e5ca3eb83df371346f7e
Vivek Suthar <sutharvivek18@gmail.com>
2024-10-10 19:08:43 +0530

    initial setup

--- helius.ts
+++
@@ -1,7 +0,0 @@
-import { Helius } from "helius-sdk";
-
-const helius = new Helius("YOUR_API_KEY");
-const response = await helius.rpc.getAssetsByOwner({
-  ownerAddress: "86xCnPeV69n6t3DnyGvkKobf9FdN2H9oiVDdaMpo2MMY",
-  page: 1,
-});
